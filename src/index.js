fac27fc437ba36cf801685e48042bbe53802aa03
Jamelele <Jamelele@users.noreply.github.com>
2017-02-20 21:45:07 +0000

    Remove console statements

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ try {
   const config = require('./config.json');
 } catch (e) {
   const config = {};
-  console.log("No config found");
 }
 
 const client = new Discord.Client();
@@ -12,8 +11,6 @@ const client = new Discord.Client();
 const token = process.env.DISCORD_TOKEN || config.discord.token;
 
 client.on('ready', function() {
-  console.log("Bot ready");
-
   client.user.setGame('SquidBot');
 });
 
