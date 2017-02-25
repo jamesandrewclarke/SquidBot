e6f59d914027ffc0b5a70477462634fee37f2221
James <jamesc2400@gmail.com>
2017-02-25 17:15:46 +0000

    Update presence message

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const client = new Discord.Client();
 const token = process.env.DISCORD_TOKEN || config.discord.token;
 
 client.on('ready', function() {
-  client.user.setGame('SquidBot');
+  client.user.setGame('SquidBot | By Jamelele');
 });
 
 client.on('message', function(message) {
