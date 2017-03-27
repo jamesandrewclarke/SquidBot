a43589aa0a3f2ff3d96597b408f557461484e4f5
James <jamesc2400@gmail.com>
2017-03-27 18:36:37 +0100

    Fixed file path again

--- src/util/RandomWord.js
+++ src/util/RandomWord.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const nounlist = fs.readFileSync('../nounlist.txt').toString().split('\n');
+const nounlist = fs.readFileSync('./nounlist.txt').toString().split('\n');
 
 module.exports = function() {
   return nounlist[Math.floor(Math.random() * nounlist.length)];
