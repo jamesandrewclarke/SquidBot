3f9b6283e21c1836b4050bb0eaafe2df9bac54a7
James <jamesc2400@gmail.com>
2017-03-27 17:57:56 +0100

    Add random word file in util folder

---
+++ src/util/RandomWord.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const nounlist = fs.readFileSync('./nounlist.txt').toString().split('\n');
+
+module.exports = function() {
+  return nounlist[Math.floor(Math.random() * nounlist.length)];
+}
