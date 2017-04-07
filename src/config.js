07f4d62f5e328cd70aee05d87815ae47fe28fd8c
James <jamesc2400@gmail.com>
2017-04-07 12:24:48 +0100

    Added guild id setting

--- src/config.js
+++ src/config.js
@@ -6,7 +6,8 @@ const config = {
   TOKEN: process.env.DISCORD_TOKEN || settings.discord.token,
   CHANNEL_ID: process.env.CHANNEL || settings.discord.channel,
   COMMAND_PREFIX: '-',
-  PRESENCE_MSG: process.env.PRESENCE_MSG || 'SquidBot | By Jamelele'
+  PRESENCE_MSG: process.env.PRESENCE_MSG || 'SquidBot | By Jamelele',
+  GUILD_ID: process.env.GUILD_ID || settings.discord.guild_id
 }
 
 module.exports = config;
