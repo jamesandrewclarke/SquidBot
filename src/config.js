2cb89bdd0568f7db7d2622bc226448bc8c61a622
James <jamesc2400@gmail.com>
2017-04-11 16:51:10 +0100

    Change command prefix

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ try { settings = require('../config.json'); } catch (e) { settings = {};}
 const config = {
   TOKEN: process.env.DISCORD_TOKEN || settings.discord.token,
   CHANNEL_ID: process.env.CHANNEL || settings.discord.channel,
-  COMMAND_PREFIX: '-',
+  COMMAND_PREFIX: ';',
   PRESENCE_MSG: process.env.PRESENCE_MSG || 'SquidBot | By Jamelele',
   GUILD_ID: process.env.GUILD_ID || settings.discord.guild_id
 }
